6fddcc820034ce99f55b6e6ec6ac43f047ee16e8
Harrisonkamau <kamauharrison87@gmail.com>
2018-01-30 08:53:15 +0300

    Fix pylint issues  - plotting should work now

--- census.py
+++ census.py
@@ -11,28 +11,16 @@ def dataset():
     """ Reads csv file & adds key-value pairs to the data """
     with open("adult.csv", mode='r') as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=KEYS)
-        print reader
-        # print filter(lambda row: row['native-country'] == 'United-States', reader)
-
-        # for row in filter(lambda row: row["native-country"] == "United-States", reader):
-        #     print(len(row))
-        # for row in filter(lambda row: row["native-country"] == "United-States", reader):
-        #     print row
-            #  print (len(row))
-            #  yield row
-        #row = [ print(i) for i in reader]
-        #print(len(row))row
-        # for row in filter(lambda row: row['native-country'] == 'United-States', reader):
-        #     print len(row)
+        filtered_data = [item for item in reader if filter(lambda row: row == 'United-States', reader)]
+        print filtered_data
 
 print dataset()
 
 def main():
     """ gets the data returned by dataset() and uses matplotlib to plot the chart """
-    # data = [(int(row[" education-num"]), row["education"])
-    #         for row in dataset(path)]
-    # plt.plot([d[1] for d in data], [d[0] for d in data], 'ro')
+    data = [(int(row[" education-num"]), row["education"])
+            for row in dataset()]
+    plt.plot([d[1] for d in data], [d[0] for d in data], 'ro')
     plt.show()
-    print(dataset())
 
 main()
